92c4f146e454724d7b5c91fcdcfbd62e932eb09c
satyen mori <satyenmori@gmail.com>
2024-03-11 11:31:45 +0530

    add jwt token

--- controller/Auth.js
+++ controller/Auth.js
@@ -4,7 +4,11 @@ export const createUser = async (req, res) => {
   try {
     const user = new User(req.body);
     const doc = await user.save();
-    res.status(200).json({ msg: "Data Saved" });
+    res.status(200).json({
+      msg: "Data Saved",
+      token: await doc.genrateToken(),
+      userId: doc._id.toString(),
+    });
   } catch (error) {
     res.status(500).json(error);
   }
@@ -15,7 +19,13 @@ export const checkUser = async (req, res) => {
     const { email, password } = req.body;
     const isUser = await User.findOne({ email, password });
     if (isUser) {
-      res.status(200).json({ msg: "login Successfuly" });
+      res
+        .status(200)
+        .json({
+          msg: "login Successfuly",
+          token: await isUser.genrateToken(),
+          userId:isUser._id.toString(),
+        });
     } else {
       res.status(401).json({ msg: " User Not Exist plz Register" });
     }
--- model/User.js
+++ model/User.js
@@ -1,12 +1,30 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 
-const userSchema=new mongoose.Schema({
-    username:{type:String , require:true},
-    email:{type:String, require:true,unique:true},
-    password:{type:String ,require:true},
-    phone:{type:Number,require:true},
-})
+const userSchema = new mongoose.Schema({
+  username: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  phone: { type: Number, require: true },
+});
 
-const User=new mongoose.model("User",userSchema)
+userSchema.methods.genrateToken = async function () {
+  try {
+    return jwt.sign(
+      {
+        userId: this._id.toString(),
+        email: this.email,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "30d",
+      }
+    );
+  } catch (error) {
+    console.error("Token Error", error);
+  }
+};
+
+const User = new mongoose.model("User", userSchema);
 
 export default User;
