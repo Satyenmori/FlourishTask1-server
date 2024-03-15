e0d83dd6fab91a1e0286cc9f5b499d76b7ddb14f
satyen mori <satyenmori@gmail.com>
2024-03-15 16:42:28 +0530

    Booking Page fetch image byID

--- controller/Rooms.js
+++ controller/Rooms.js
@@ -10,3 +10,15 @@ export const fetchAllroom = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+// fetch Rooms by Id
+
+export const fetchRoomsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const roomsById = await Room.findById(id);
+    res.status(200).json(roomsById);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- router/Rooms.js
+++ router/Rooms.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { fetchAllroom } from "../controller/Rooms.js";
+import { fetchAllroom, fetchRoomsById } from "../controller/Rooms.js";
 
 export const roomRouter = express.Router();
 
-roomRouter.get("/", fetchAllroom);
+roomRouter.get("/", fetchAllroom).get("/:id", fetchRoomsById);
